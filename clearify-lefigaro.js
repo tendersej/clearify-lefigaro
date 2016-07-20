7af614b68c1c4e85d94ef8137206e65f6c86e274
Anmi <dnfl@users.noreply.github.com>
2016-07-20 14:09:32 +0200

    no message

--- clearify-lefigaro.js
+++ clearify-lefigaro.js
@@ -1,1 +1,14 @@
 javascript:var clearify = function(){document.getElementsByClassName('fig-article-body')[0].style.textShadow = 'None';document.getElementsByClassName('fig-article-body')[0].style.color = 'black';var imgToBeDeleted = document.querySelector('.fig-article-body a img');imgToBeDeleted.parentNode.removeChild(imgToBeDeleted);};clearify();
+
+
+// for readability
+// Improvement opportunities:
+//  declaring a var instead of calling "document.getElementsByClassName('fig-article-body')[0]" twice
+//
+// javascript:var clearify = function(){
+//     document.getElementsByClassName('fig-article-body')[0].style.textShadow = 'None';
+//     document.getElementsByClassName('fig-article-body')[0].style.color = 'black';
+//     var imgToBeDeleted = document.querySelector('.fig-article-body a img');
+//     imgToBeDeleted.parentNode.removeChild(imgToBeDeleted);
+// };
+// clearify();
